b3da6352ec3b824d0369b306b97a5c02062a4165
Dani <daniprgm@gmail.com>
2025-06-23 15:13:57 +0200

    sitemap-index.xml añadido

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 // astro.config.mjs
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
+  integrations: [
+    sitemap({ changefreq: 'daily', priority: 0.8 }),
+  ],
 });
 
